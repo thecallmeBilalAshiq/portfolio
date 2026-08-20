ea068cbe1609931b2586436f14824972e183e317
Muhammad Bilal <138978969+thecallmeBilalAshiq@users.noreply.github.com>
2026-08-20 17:57:27 +0000

    Revert "Merge pull request #3 from thecallmeBilalAshiq/revert-2-revert-1-terminal-portfolio-migration-7668573676895315365"
    This reverts commit e464dca68533eea8abb5d674114c0ae3c7fc1e6a, reversing
    changes made to ff08e6e63058f7b5ab8377f1e87ecfaf31ec33dc.

--- app/page.js
+++ app/page.js
@@ -1,9 +1,92 @@
-import Terminal from '@/components/Terminal';
+'use client';
+
+import { useEffect } from 'react';
+import Navbar from '@/components/Navbar';
+import HeroSection from '@/sections/HeroSection';
+import AboutSection from '@/sections/AboutSection';
+import ExperienceSection from '@/sections/ExperienceSection';
+import EducationSection from '@/sections/EducationSection';
+import ProjectsSection from '@/sections/ProjectsSection';
+import SkillsSection from '@/sections/SkillsSection';
+import VolunteerSection from '@/sections/VolunteerSection';
+import CertificatesSection from '@/sections/CertificatesSection';
+import GalleryMarqueeSection from '@/sections/GalleryMarqueeSection';
+import ContactSection from '@/sections/ContactSection';
+import FooterSection from '@/sections/FooterSection';
+import CustomCursor from '@/components/CustomCursor';
+import ScrollToTop from '@/components/ScrollToTop';
+import FloatingChatbot from '@/components/FloatingChatbot';
+import BackgroundCanvas from '@/components/BackgroundCanvas';
+import ScrollProgress from '@/components/ScrollProgress';
+import Lenis from 'lenis';
 
 export default function Home() {
+  useEffect(() => {
+    // Initialize Lenis Smooth Scroll
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      smoothWheel: true,
+      smoothTouch: false
+    });
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    // Initialize AOS Animation Library
+    const initAOS = () => {
+      if (typeof window !== 'undefined' && window.AOS) {
+        window.AOS.init({
+          duration: 1000,
+          once: true,
+          offset: 100
+        });
+      }
+    };
+
+    if (window.AOS) {
+      initAOS();
+    } else {
+      const interval = setInterval(() => {
+        if (window.AOS) {
+          initAOS();
+          clearInterval(interval);
+        }
+      }, 200);
+      return () => clearInterval(interval);
+    }
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
   return (
-    <main>
-      <Terminal />
+    <main className="main-content">
+      <ScrollProgress />
+      <BackgroundCanvas />
+      <CustomCursor />
+      <Navbar />
+
+      <HeroSection />
+      <AboutSection />
+      <ExperienceSection />
+      <EducationSection />
+      <ProjectsSection />
+      <SkillsSection />
+      <VolunteerSection />
+      <CertificatesSection />
+      <GalleryMarqueeSection />
+      <ContactSection />
+
+      <FooterSection />
+
+      <ScrollToTop />
+      <FloatingChatbot />
     </main>
   );
 }
